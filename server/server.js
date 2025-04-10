f325a137d7f6f60219b4f96b6baaf46f701bce1e
sanyuchilas <sanyuchilas@gmail.com>
2025-04-10 17:39:51 +0300

    added docker

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const PORT = 5173;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const distPath = path.join(__dirname, "../dist");
+
+app.use(express.static(distPath));
+
+app.get("*", (req, res) => {
+  return res.sendFile(path.join(distPath, "index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Front listening on PORT: ${PORT}...`);
+});
